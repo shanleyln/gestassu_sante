cf4edd005124637d24d2933bc895dbcf25a78596
MOULOUNGUI-Bienvenu <moulounguibienvenuesamara@gmail.com>
2025-05-20 17:53:00 +0200

    nesta

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,20 +2,27 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
+     * Le modèle n'utilise pas d'auto-incrémentation
+     */
+    public $incrementing = false;
+
+    /**
+     * La clé primaire est une chaîne (UUID)
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Mass assignable attributes.
      */
     protected $fillable = [
         'name',
@@ -24,9 +31,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
+     * Hidden attributes.
      */
     protected $hidden = [
         'password',
@@ -34,9 +39,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
+     * Casts.
      */
     protected function casts(): array
     {
@@ -44,5 +47,19 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Boot method pour générer un UUID automatiquement
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
     }
 }
